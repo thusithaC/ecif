2626292246e74999fa0c2cc4519cb45ad4cfff7c
Thusitha Chandrapala <thusithanc@gmail.com>
2023-10-17 21:57:24 +1100

    fix formatting

--- ecif/configs/base_trial_config.py
+++ ecif/configs/base_trial_config.py
@@ -1,14 +1,18 @@
 """Config object for a Trial."""
-from pydantic import BaseModel, ValidationError, model_validator
 from enum import Enum
+
+from pydantic import BaseModel, ValidationError, model_validator
 
 
 class CalculationMethod(Enum):
+    """Enumeration for calculation method."""
+
     CLASSICAL_DID_REGRESSION = "classical-did-regression"
 
 
 class DataModel(BaseModel):
     """Data setup for the trial."""
+
     target_column: str
     treatment_column: str
     control_variable_columns: None | list[str]
@@ -16,6 +20,7 @@ class DataModel(BaseModel):
 
 class CalculationModel(BaseModel):
     """Calculation model for the trial."""
+
     method: CalculationMethod
     equation: str
     estimate_intervals: bool
@@ -23,19 +28,18 @@ class CalculationModel(BaseModel):
 
     @model_validator(mode="after")
     def is_interval_defined(self):
+        """Validate if interval level is defined if estimate_intervals."""
         if self.estimate_intervals is True:
             if self.interval_level is None:
-                raise ValidationError("interval_level must be defined if estimate_intervals is True")
+                raise ValidationError(
+                    "interval_level must be defined if estimate_intervals is True"
+                )
         return self
 
 
 class BaseTrialConfig(BaseModel):
     """General configuration for a trial."""
+
     trial_name: str
     data: DataModel
     calculation_model: CalculationModel
-
-
-
-
-
